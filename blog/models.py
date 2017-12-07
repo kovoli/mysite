5ac70554402dc47725f7f56385aa68de2f19901b
Imac <imac@Imacs-iMac.fritz.box>
2017-12-07 21:36:58 +0100

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
