7232a5d7e76ed054df4c6c32df2a83ce821ea7a6
kovoli <kovoli1985@gmail.com>
2017-12-08 20:58:14 +0100

    models.py noch nicht fertig

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,7 @@ class Post(models.Model):
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_posts')
+    body = models.TextField()
