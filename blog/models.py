d0a84eca205da7af007037add74c88b5ee9f679a
kovoli <kovoli1985@gmail.com>
2017-12-09 21:16:05 +0100

    models fertig gemacht

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,15 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, related_name='blog_posts')
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)  # дата будет автоматически добавлена при создании объекта
+    update = models.DateTimeField(auto_now=True)  # дата будет автоматически обновляться при сохранении объекта
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+
+class Meta:
+    ordering = ('-publish',)  # сортировать результаты по полю publish
+
+
+def __str__(self):
+    return self.title
